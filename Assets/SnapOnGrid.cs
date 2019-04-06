9e0570c9b64fd53c11238762d200cd75438416cb
DESKTOP-4QB4TVC\Diego Belini <drxbelini@gmail.com>
2019-04-06 14:21:25 -0300

    Set text mesh to inform the drig position or cordenates.

--- Assets/SnapOnGrid.cs
+++ Assets/SnapOnGrid.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
+
 public class SnapOnGrid : MonoBehaviour
 {
     [SerializeField] [Range(1f,20f)] float gridSeize = 10f;
-  
+
+    TextMesh textMesh;
+    
+
     // Update is called once per frame
     void Update()
     {
@@ -16,6 +21,9 @@ public class SnapOnGrid : MonoBehaviour
         SnapPos.z = Mathf.Round(transform.position.z / gridSeize) * gridSeize;
 
         transform.position = new Vector3(SnapPos.x, 0f, SnapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = SnapPos.x / gridSeize + "," + SnapPos.z / gridSeize;
 
 
     }
