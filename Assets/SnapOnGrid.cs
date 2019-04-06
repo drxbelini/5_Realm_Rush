46aeb9713f183feb7fe78e06f953c09c7c0f0a8c
DESKTOP-4QB4TVC\Diego Belini <drxbelini@gmail.com>
2019-04-05 23:32:21 -0300

    First Commit Create cube with quad Creat script for snap on grid

---
+++ Assets/SnapOnGrid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapOnGrid : MonoBehaviour
+{
+    [SerializeField] [Range(1f,20f)] float gridSeize = 10f;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 SnapPos;
+
+        SnapPos.x = Mathf.Round(transform.position.x / gridSeize) * gridSeize;
+        SnapPos.z = Mathf.Round(transform.position.z / gridSeize) * gridSeize;
+
+        transform.position = new Vector3(SnapPos.x, 0f, SnapPos.z);
+
+
+    }
+}
