7367a605e1962ff7f2aff219b32266cfbd23e8e5
DESKTOP-4QB4TVC\Diego Belini <drxbelini@gmail.com>
2019-05-16 12:03:36 -0300

    Post Processing FX

---
+++ Assets/Scripts/PlayerHelth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHelth : MonoBehaviour
+{
+    [SerializeField] int lifePoints = 50000;
+    [SerializeField] int damagePoints = 1000;
+    [SerializeField] Text helthText;
+
+    private void Start()
+    {
+        helthText.text = lifePoints.ToString();
+    }
+    public void HelthDecreacer()
+    {
+        lifePoints = lifePoints - damagePoints;
+        helthText.text = lifePoints.ToString();
+
+        if (lifePoints <= 0)
+        {
+            deathSequence();
+        }
+    }
+
+    private void deathSequence()
+    {
+        helthText.text = "Base destroyed";
+        print("your death");
+        Destroy(gameObject);
+    }
+}
